5f8a1da89d894f5f07fb8aa20def775acb804cca
DESKTOP-PV7DVD9\KGA_17 <tyhbvfg0108@gmail.com>
2020-07-24 10:19:08 +0900

    UI 안올라간 거 있어서 마저 올립니다

---
+++ InGameUI.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "gameNode.h"
+
+class BossProgressBar;
+
+class InGameUI : public gameNode
+{
+private:
+	BossProgressBar* _BossProgressBar;
+
+	int _PlayerHP;
+	float _KyokoX, _MisuzuX, _KyokoY, _MisuzuY;
+	int _BattleIntroAlphaValue;
+	int _ScreenLockAlphaValue;
+	int _BattleIntroCount;
+	bool _StageToggle;
+
+/// <summary>
+	int _SimulationValue;
+/// </summary>
+
+public:
+	InGameUI();
+	~InGameUI();
+
+	HRESULT init();
+	virtual void release();
+	virtual void update();
+	virtual void render();
+
+};
+
